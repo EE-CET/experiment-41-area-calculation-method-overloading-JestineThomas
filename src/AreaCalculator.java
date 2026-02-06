b70a53bcd96ef4fff0823fbb3d7505fb6e9b1904
jestiii <jestinethomas556@gmail.com>
2026-02-05 20:27:00 -0800

    added

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -3,30 +3,33 @@ import java.util.Scanner;
 public class AreaCalculator {
     
     static float calculateArea(float radius){
-        return (3.14159f*radius*radius);
+        return 3.14159f * radius * radius;
     }
     
-    static float calcluateArea(float length,float breadth){
-        return (length * breadth);
+    static float calculateArea(float length, float breadth){
+        return length * breadth;
     }
     
-    static double calculateArea(double base,double height){
-        return (0.5 * base * height);
+    static double calculateArea(double base, double height){
+        return 0.5 * base * height;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter radius of circle:" );
-        float r =sc.nextFloat();
-        System.out.println("enter length and Breadth of circle:" );
-        float l =sc.nextFloat();
-        float b =sc.nextFloat();
-        System.out.println("enter base and height of circle:" );
-        double base =sc.nextDouble();
-        double height =sc.nextDouble();
+
+        System.out.println("Enter radius of circle:");
+        float r = sc.nextFloat();
+
+        System.out.println("Enter length and breadth of rectangle:");
+        float l = sc.nextFloat();
+        float b = sc.nextFloat();
+
+        System.out.println("Enter base and height of triangle:");
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
         
         System.out.printf("%.2f\n", calculateArea(r));
-        System.out.printf("%.2f\n", calculateArea(l,b));
-        System.out.printf("%.2f\n", calculateArea(base,height));
+        System.out.printf("%.2f\n", calculateArea(l, b));
+        System.out.printf("%.2f\n", calculateArea(base, height));
     }
 }
